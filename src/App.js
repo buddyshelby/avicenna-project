566ef91ee8067369749f32af2d9280b25835edbc
buddyshelby <mtegar057@gmail.com>
2023-09-17 18:58:23 +0700

    Update Sun Sep 17 18:58:00 UTC 2023

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './App.css';
 import Login, { action as loginAction } from './views/Login/Login'
-import Navbar from './components/navbar/Navbar'
+import Signup from './views/Signup/Signup'
+import Dashboard, { action as dashboardAction } from './views/Dashboard/Dashboard'
+// import Navbar from './components/navbar/Navbar'
 import RootLayout from './pages/RootPage'
 import ErrorPage from './views/Error/ErrorPage'
 
@@ -11,11 +13,21 @@ const router = createBrowserRouter([
     element: <RootLayout />,
     errorElement: <ErrorPage />,
     children: [
-      { index: true, element: <Login />,action: loginAction, },
+      { index: true, element: <Login />, action: loginAction, },
       {
         path: 'login',
         element: <Login />,
         action: loginAction,
+      },
+      {
+        path: 'signup',
+        element: <Signup />,
+        // action: loginAction,
+      },
+      {
+        path: 'dashboard',
+        element: <Dashboard />,
+        action: dashboardAction,
       },
     ],
   },
--- src/views/Login/Login.js
+++ src/views/Login/Login.js
@@ -1,10 +1,11 @@
 import desktopStyles from './login.module.css'
 import mobileStyles from './mobile-login.module.css'
 import loginJson from '../../JSON/login.json'
+import Navbar from '../../components/navbar/Navbar'
 import imageStorage from '../../assets/Login/imageStorage'
 import ShadowBackground from '../../components/background/ShadowBackground'
 import SeparateFunction, { ResponsiveComponent, useWindowSize } from '../../Function/SeparateFunction'
-import { useFetcher  } from 'react-router-dom';
+import { useFetcher } from 'react-router-dom';
 import { useEffect, useRef, useState } from 'react';
 
 const Login = () => {
@@ -30,7 +31,7 @@ const Login = () => {
     }
 
     useEffect(() => {
-        console.log(windowWidth < 600);
+        // console.log(windowWidth < 600);
         if (windowWidth > 600) {
             setStyles(desktopStyles)
             setIsMobile(false)
@@ -43,42 +44,51 @@ const Login = () => {
     return (
         <main id={styles['login--page']}>
             <SeparateFunction />
-                    {isMobile &&
-                    <ShadowBackground showModal={showModal}>
-                        <div className={styles['login--content-right']} onClick={closeModal}>
-                            <fetcher.Form method='post' action='/login' className={styles['login--content-form']} style={{ width: `${ResponsiveComponent(600, 300, 280, 150, windowWidth)}px` }}>
-                                <div className={styles['login--content-form--title']}>
-                                    <h4 style={{ fontSize: `${ResponsiveComponent(600, 32, 280, 22, windowWidth)}px` }}>{loginFormJson.title}</h4>
-                                </div>
-                                <div className={styles['login--content-form--input-email']}>
-                                    <span><img src={imageStorage.emailIcon} alt="" /></span>
-                                    <input type="email" name="email" placeholder={loginFormJson.input_1} autoComplete="email" style={{ fontSize: `${ResponsiveComponent(600, 14, 280, 7, windowWidth)}px` }} />
-                                </div>
-                                <div className={styles['login--content-form--input-password']}>
-                                    <span><img src={imageStorage.passIcon} alt="" /></span>
-                                    <input type="password" name="password" placeholder={loginFormJson.input_2} autoComplete="current-password" style={{ fontSize: `${ResponsiveComponent(600, 14, 280, 7, windowWidth)}px` }} />
-                                </div>
-                                <div className={styles['login--content-form--remember-me']}>
-                                    <div className={styles['login--content-form--remember-me--radio-button']} onClick={() => setIsRemember(!isRemember)} style={{ backgroundColor: isRemember ? `#5669FF` : `transparent`, border: `1px solid ${isRemember ? '#5669FF' : '#fff'}` }}>
-                                        <div style={{ height: `${ResponsiveComponent(600, 15, 280, 7.5, windowWidth)}px`, transform: isRemember ? `translateX(100%)` : `translateX(0)` }}></div>
-                                    </div>
-                                    <span style={{ fontSize: `${ResponsiveComponent(600, 12, 280, 5, windowWidth)}px` }}>{loginFormJson.span_1}</span>
-                                </div>
-                                <div className={styles['login--content-form--input-button']}>
-                                    <div>
-                                        <div>
-                                            <input type="submit" value={loginFormJson.button_1} ref={subButton} style={{ fontSize: `${ResponsiveComponent(1080, 13, 600, 10, windowWidth)}px` }} />
-                                            <div style={{ left: `${ResponsiveComponent(600, 85, 280, 80, windowWidth)}%` }}>
-                                                <span className="material-icons" onClick={handleSubmit}>
-                                                    arrow_circle_right
-                                                </span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </fetcher.Form>
+            {isMobile &&
+            <ShadowBackground showModal={showModal}>
+                <div className={styles['login--content-right']} onClick={closeModal}>
+                    <fetcher.Form method='post' action='/login' className={styles['login--content-form']} style={{ width: `${ResponsiveComponent(600, 300, 280, 150, windowWidth)}px` }}>
+                        <div className={styles['login--content-form--title']}>
+                            <h4 style={{ fontSize: `${ResponsiveComponent(600, 32, 280, 22, windowWidth)}px` }}>{loginFormJson.title}</h4>
                         </div>
-                    </ShadowBackground>}
+                        <div className={styles['login--content-form--input-email']}>
+                            <span><img src={imageStorage.emailIcon} alt="" /></span>
+                            <input type="email" name="email" placeholder={loginFormJson.input_1} autoComplete="email" style={{ fontSize: `${ResponsiveComponent(600, 14, 280, 7, windowWidth)}px` }} />
+                        </div>
+                        <div className={styles['login--content-form--input-password']}>
+                            <span><img src={imageStorage.passIcon} alt="" /></span>
+                            <input type="password" name="password" placeholder={loginFormJson.input_2} autoComplete="current-password" style={{ fontSize: `${ResponsiveComponent(600, 14, 280, 7, windowWidth)}px` }} />
+                        </div>
+                        <div className={styles['login--content-form--forget--sign-up']}>
+                            <div className={styles['login--content-form--forget-password']}>
+                                <span style={{ fontSize: `${ResponsiveComponent(600, 12, 280, 5, windowWidth)}px` }}>{loginFormJson.span_1}</span>
+                            </div>
+                            <div className={styles['login--content-form--sign-up']}>
+                                <span style={{ fontSize: `${ResponsiveComponent(600, 12, 280, 5, windowWidth)}px` }}>{loginFormJson.span_2}</span>
+                            </div>
+                        </div>
+                        <div className={styles['login--content-form--remember-me']}>
+                            <div className={styles['login--content-form--remember-me--radio-button']} onClick={() => setIsRemember(!isRemember)} style={{ backgroundColor: isRemember ? `#5669FF` : `transparent`, border: `1px solid ${isRemember ? '#5669FF' : '#fff'}` }}>
+                                <div style={{ height: `${ResponsiveComponent(600, 15, 280, 7.5, windowWidth)}px`, transform: isRemember ? `translateX(100%)` : `translateX(0)` }}></div>
+                            </div>
+                            <span style={{ fontSize: `${ResponsiveComponent(600, 12, 280, 5, windowWidth)}px` }}>{loginFormJson.span_1}</span>
+                        </div>
+                        <div className={styles['login--content-form--input-button']}>
+                            <div>
+                                <div>
+                                    <input type="submit" value={loginFormJson.button_1} ref={subButton} style={{ fontSize: `${ResponsiveComponent(1080, 13, 600, 10, windowWidth)}px` }} />
+                                    <div style={{ left: `${ResponsiveComponent(600, 85, 280, 80, windowWidth)}%` }}>
+                                        <span className="material-icons" onClick={handleSubmit}>
+                                            arrow_circle_right
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </fetcher.Form>
+                </div>
+            </ShadowBackground>}
+            <Navbar />
             <section id={styles.login} style={{ '--loginBG': `url(${imageStorage.loginBG})` }}>
                 <div className={styles['login--container']}>
                     <div className={styles['login--wrapper']}>
@@ -112,11 +122,19 @@ const Login = () => {
                                     <span><img src={imageStorage.passIcon} alt="" /></span>
                                     <input type="password" name="password" placeholder={loginFormJson.input_2} autoComplete="current-password" style={{ fontSize: !isMobile && `${ResponsiveComponent(1080, 14, 600, 7, windowWidth)}px` }} />
                                 </div>
+                                <div className={styles['login--content-form--forget--sign-up']}>
+                                    <div className={styles['login--content-form--forget-password']}>
+                                        <span style={{ fontSize: !isMobile && `${ResponsiveComponent(1080, 12, 600, 5, windowWidth)}px` }}>{loginFormJson.span_1}</span>
+                                    </div>
+                                    <div className={styles['login--content-form--sign-up']}>
+                                        <span style={{ fontSize: !isMobile && `${ResponsiveComponent(1080, 12, 600, 5, windowWidth)}px` }}>{loginFormJson.span_2}</span>
+                                    </div>
+                                </div>
                                 <div className={styles['login--content-form--remember-me']}>
                                     <div className={styles['login--content-form--remember-me--radio-button']} onClick={() => setIsRemember(!isRemember)} style={{ backgroundColor: isRemember ? `#5669FF` : `transparent`, border: `1px solid ${isRemember ? '#5669FF' : '#fff'}` }}>
                                         <div style={{ height: !isMobile && `${ResponsiveComponent(1080, 15, 600, 7.5, windowWidth)}px`, transform: isRemember ? `translateX(100%)` : `translateX(0)` }}></div>
                                     </div>
-                                    <span style={{ fontSize: !isMobile && `${ResponsiveComponent(1080, 12, 600, 5, windowWidth)}px` }}>{loginFormJson.span_1}</span>
+                                    <span style={{ fontSize: !isMobile && `${ResponsiveComponent(1080, 12, 600, 5, windowWidth)}px` }}>{loginFormJson.span_3}</span>
                                 </div>
                                 <div className={styles['login--content-form--input-button']}>
                                     <div>
